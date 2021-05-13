5c04aaa000ed80146866b06ef99e2aebc6795319
daniel-dantas <daniel.dantas.ip19@hotmail.com>
2021-05-13 20:36:53 -0300

    Correção de problemas

--- src/pages/Cadastro/index.js
+++ src/pages/Cadastro/index.js
@@ -1,32 +1,34 @@
 import React, {useState} from 'react';
-import { View, Text, InputText, Button } from 'react-native';
-import { RectButton, TextInput } from 'react-native-gesture-handler';
+import {View, Text, TouchableOpacity} from 'react-native';
+import {TextInput} from 'react-native-gesture-handler';
 import api from '../../services/api'
+
 export default function Cadastro() {
-
-
 
     const [nome, setNome] = useState("");
     const [email, setEmail] = useState("");
 
-    async function Cadastroalunos(nome, email){
-          try{
-            await api.post(`discentes`, [
-                nome,
+    async function Cadastroalunos(name, email) {
+        try {
+            await api.post(`discentes`, {
+                name,
                 email,
-            ])
+                password: "teste"
+            })
             alert("Adcionado !")
-        }catch(err){
+        } catch (err) {
             alert('erro ao Adcionar');
         }
     }
+
     return (
-        <><View>
-            <Text>Cadastro de Aluno</Text>
-            <TextInput
+        <>
+            <View>
+                <Text>Cadastro de Aluno</Text>
+                <TextInput
                     placeholder='Nome'
                     placeholderTextColor="#aaaaaa"
-                    onChangeText={ nome => setNome(nome)}
+                    onChangeText={nome => setNome(nome)}
                     value={nome}
                     underlineColorAndroid="transparent"
                     autoCapitalize="none"
@@ -34,12 +36,16 @@ export default function Cadastro() {
                 <TextInput
                     placeholder='E-mail'
                     placeholderTextColor="#aaaaaa"
-                    onChangeText={ email => setEmail(email)}
+                    onChangeText={email => setEmail(email)}
                     value={email}
                     underlineColorAndroid="transparent"
                     autoCapitalize="none"
                 />
-                <Button onPress={() => {Cadastroalunos(nome, email)}} >Cadastrar</Button>
-        </View></>
+                <TouchableOpacity onPress={() => {
+                    Cadastroalunos(nome, email).then()
+                }}>
+                    <Text>Cadastrar</Text></TouchableOpacity>
+            </View>
+        </>
     )
 }
--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://localhost:8000/api/v1/',
+    //Aqui você colocar o IP que mostra quando você executa o expo start
+    baseURL: 'http://192.168.1.115:8000/api/v1/',
 })
 
 export default api;
